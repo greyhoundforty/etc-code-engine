50a88b919370ac1a3ad817d46a509594fffd4399
greyhoundforty <ryantiffany@fastmail.com>
2023-03-06 16:31:03 -0600

    Testing updated resource pull

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sys
 import json
 
 etcdConnection = os.environ.get('CE_SERVICES')
-print(type(etcdConnection))
+etcdDetails = etcdConnection[0]
+# print(type(etcdConnection))
+print(etcdDetails)
+
 
 ## print(json.dumps(etcdConnection, indent=2))
