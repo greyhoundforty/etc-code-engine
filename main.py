32b092c9da19a2ff9e242c604de9233f1b890a79
greyhoundforty <ryantiffany@fastmail.com>
2023-03-07 09:00:16 -0600

    Pulling all env vars from Code engine project

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import os
 import sys
 import json
 
-# etcdConnection = os.environ.get('CE_SERVICES')
-# # etcdDetails = etcdConnection[0]
-# # # print(type(etcdConnection))
-# print(etcdDetails)
+ceServicesVar = os.environ.get('CE_SERVICES')
+etcdServiceVar = os.environ.get('DATABASES_FOR_ETCD_CONNECTION')
 
+jls_extract_var = type
+print("CE Services environment variable is type: " + jls_extract_var(ceServicesVar))
+print("CE Services environment variable value is: " + ceServicesVar)
 
-# etcdDetails =  print(json.dumps(etcdConnection, indent=2))
-
-for name, value in os.environ.items():
-    print("{0}: {1}".format(name, value))
+print("ETCD Services environment variable is type: " + jls_extract_var(etcdServiceVar))
+print("ETCD Services environment variable value is: " + etcdServiceVar)
