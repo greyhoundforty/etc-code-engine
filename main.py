cefa9d14cae37e8bfb26378108021b0aee1bd267
greyhoundforty <ryantiffany@fastmail.com>
2023-03-07 09:11:30 -0600

    Pulling etcd var type

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import json
 ceServicesVar = os.environ.get('CE_SERVICES')
 etcdServiceVar = os.environ.get('DATABASES_FOR_ETCD_CONNECTION')
 
-print("CE Services environment variable is type")
+print("CE services env var type")
 print(type(ceServicesVar))
-print("CE Services environment variable value is: " + ceServicesVar)
+print("CE services env var value: " + ceServicesVar)
 
-print("ETCD Services environment variable is type: " + type(etcdServiceVar))
-print("ETCD Services environment variable value is: " + etcdServiceVar )
+print("ETCD env var type")
+print(type(etcdServiceVar))
+print("ETCD env var value: " + etcdServiceVar)
