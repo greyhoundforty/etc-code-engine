b87c15786432f17ea768370b717fb5d7140f571f
greyhoundforty <ryantiffany@fastmail.com>
2023-03-07 09:25:52 -0600

    update

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ import json
 ceServicesVar = os.environ.get('CE_SERVICES')
 etcdServiceVar = os.environ.get('DATABASES_FOR_ETCD_CONNECTION')
 
-print("CE services env var type")
-print(type(ceServicesVar))
-print("CE services env var value: " + ceServicesVar)
+# print("CE services env var type")
+# print(type(ceServicesVar))
+# print("CE services env var value: " + ceServicesVar)
 
 print("ETCD env var type")
-print(type(etcdServiceVar))
-print("ETCD env var value: " + etcdServiceVar)
+json_object = json.loads(etcdServiceVar)
+print(type(json_object))
+print(json_object)
+
+
+# print(type(etcdServiceVar))
+# print("ETCD env var value: " + etcdServiceVar)
+print("seeing if we can pull all values from json object")
+for values in json_object.values():
+   print(values)
+
+
+print(json_object["composed"])
