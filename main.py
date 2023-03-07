a4f40808386c47f7930e40e7bf628a4bcf02cf10
greyhoundforty <ryantiffany@fastmail.com>
2023-03-07 13:54:19 -0600

    update

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ etcdServiceVar = os.environ.get('DATABASES_FOR_ETCD_CONNECTION')
 json_object = json.loads(etcdServiceVar)
 argVars = list(json_object.values())[0]
 connectionVars = list(json_object.values())[1]
-otherVars = list(json_object.values())[2]
 
 try:
+    print("Pulling all connection info for etcd instance")
+    print(connectionVars)
     print("Pulling hosts for etcd instance")
     print(connectionVars['hosts'])
     # print("Pulling hostname for etcd instance")
@@ -18,8 +19,7 @@ try:
     # print(connectionVars['hosts'][0]['port'])
     # print("Pulling password for etcd instance")
     print("attempting to decode cert for etcd instance")
-    decodedCert = base64.b64decode(connectionVars['hosts'][0]['certificate_base64'])
+    decodedCert = base64.b64decode(connectionVars['certificate_base64'])
     print(decodedCert)
-    print(otherVars)
 except KeyError:
     print("Key not found")
