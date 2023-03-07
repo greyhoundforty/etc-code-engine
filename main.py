bbfa7af5df08095ad6c554d69ff169d978e10af7
greyhoundforty <ryantiffany@fastmail.com>
2023-03-07 08:45:45 -0600

    Pulling all env vars from Code engine project

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import os
 import sys
 import json
 
-etcdConnection = os.environ.get('CE_SERVICES')
-etcdDetails = etcdConnection[0]
-# print(type(etcdConnection))
-print(etcdDetails)
+# etcdConnection = os.environ.get('CE_SERVICES')
+# # etcdDetails = etcdConnection[0]
+# # # print(type(etcdConnection))
+# print(etcdDetails)
 
 
-## print(json.dumps(etcdConnection, indent=2))
+# etcdDetails =  print(json.dumps(etcdConnection, indent=2))
+
+for name, value in os.environ.items():
+    print("{0}: {1}".format(name, value))
