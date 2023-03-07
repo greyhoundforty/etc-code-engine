af538d8e6563c39f25a098bb30eccbc20e0163b2
greyhoundforty <ryantiffany@fastmail.com>
2023-03-07 13:06:11 -0600

    update

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ import json
 etcdServiceVar = os.environ.get('DATABASES_FOR_ETCD_CONNECTION')
 
 json_object = json.loads(etcdServiceVar)
-argumentVars = list(json_object.values())[0]
 connectionVars = list(json_object.values())[1]
 
 try:
-    print("Pulling all argument info for etcd instance")
-    print(argumentVars)
     print("Pulling composed connection info for etcd instance")
     print(connectionVars['composed'][0])
-    print("Dumping full json object")
-    print(json_object)
+    print("Pulling hosts for etcd instance")
+    print(connectionVars['hosts'])
+    print("Pulling port for etcd instance")
+    print(connectionVars['port'])
+    print("Pulling hostname for etcd instance")
+    print(connectionVars['hosts'][0]['hostname'])
 except KeyError:
     print("Key not found")
