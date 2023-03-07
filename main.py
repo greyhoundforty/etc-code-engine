4bb6dc60e2a4fc5a082a645450017933d44a8325
greyhoundforty <ryantiffany@fastmail.com>
2023-03-07 09:03:38 -0600

    Pulling all 2 ce provided vars

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import json
 ceServicesVar = os.environ.get('CE_SERVICES')
 etcdServiceVar = os.environ.get('DATABASES_FOR_ETCD_CONNECTION')
 
-jls_extract_var = type
-print("CE Services environment variable is type: " + jls_extract_var(ceServicesVar))
+print("CE Services environment variable is type")
+print(type(ceServicesVar))
 print("CE Services environment variable value is: " + ceServicesVar)
 
-print("ETCD Services environment variable is type: " + jls_extract_var(etcdServiceVar))
-print("ETCD Services environment variable value is: " + etcdServiceVar)
+print("ETCD Services environment variable is type: " + type(etcdServiceVar))
+print("ETCD Services environment variable value is: " + etcdServiceVar )
