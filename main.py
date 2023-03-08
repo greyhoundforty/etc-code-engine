8e6257e9154659574ab9eb095551dfa1bf1190a1
greyhoundforty <ryantiffany@fastmail.com>
2023-03-08 04:56:03 -0600

    update

--- main.py
+++ main.py
@@ -12,28 +12,33 @@ connectionVars = list(json_object.values())[1]
 composedConnection = connectionVars['composed'][0]
 certDetails = connectionVars['certificate']['certificate_base64']
 
-etcdClient = etcd.Client(
-    host=connectionVars['hosts'][0]['hostname'],
-    port=connectionVars['hosts'][0]['port'],
-    username=connectionVars['authentication']['username'],
-    password=connectionVars['authentication']['password'],
-    allow_reconnect=True,
-    protocol='https',
-    ca_cert=base64.b64decode(certDetails)
-)
+# etcdClient = etcd.Client(
+#     host=connectionVars['hosts'][0]['hostname'],
+#     port=connectionVars['hosts'][0]['port'],
+#     username=connectionVars['authentication']['username'],
+#     password=connectionVars['authentication']['password'],
+#     allow_reconnect=True,
+#     protocol='https',
+#     ca_cert=base64.b64decode(certDetails)
+# )
 
 # getLeader = etcdClient.leader
 
+etcdHost = connectionVars['hosts'][0]['hostname']
+etcdPort=connectionVars['hosts'][0]['port']
+etcdUsername=connectionVars['authentication']['username']
+etcdPassword=connectionVars['authentication']['password']
+
 try:
+    print("Pulling username connection info for etcd instance")
+    print(connectionVars['authentication']['username'])
+    print("Pulling password connection info for etcd instance")
+    print(connectionVars['authentication']['password'])
+    print("Pulling hostname for etcd instance")
+    print(connectionVars['hosts'][0]['hostname'])
+    print("Pulling port for etcd instance")
+    print(connectionVars['hosts'][0]['port'])
     print(ca_cert)
-    # print("Pulling password connection info for etcd instance")
-    # print(connectionVars['authentication']['password'])
-    # print("Pulling hosts for etcd instance")
-    # print(connectionVars['hosts'])
-    # # print("Pulling hostname for etcd instance")
-    # # print(connectionVars['hosts'][0]['hostname'])
-    # # print("Pulling port for etcd instance")
-    # # print(connectionVars['hosts'][0]['port'])
     # # print("Pulling password for etcd instance")
     # print("attempting to pull cert info for etcd instance")
     # 
